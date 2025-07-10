dc5ff54f64501f999c9f68a74b61efbeb9cfe8ba
Viviana Gonzalez <vivigz06@gmail.com>
2025-07-10 16:18:01 -0300

    se hicieron las pruebas y se cambio de puerto

--- recuperatorio-segundo-parcial/app.js
+++ recuperatorio-segundo-parcial/app.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import sequelize from "./src/config/database.js";
+import languageRoutes from "./src/routes/language.routes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000; //no se si se podia cambiar el puerto, pero no me andaba el 3000
+app.use("/api/languages", languageRoutes);
 
 // importar las rutas
 sequelize.sync().then(() => {
